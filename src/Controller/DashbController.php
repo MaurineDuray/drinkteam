84e70c2a1da2501f6c87de63112f8c515c9745d3
MaurineDuray <maurine.duray@skynet.be>
2023-03-17 22:31:26 +0100

    affichage de l'admin dashboard

--- src/Controller/DashbController.php
+++ src/Controller/DashbController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
+use App\Repository\RecipesRepository;
+use App\Repository\CommentsRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DashbController extends AbstractController
 {
@@ -13,6 +16,33 @@ class DashbController extends AbstractController
     {
         return $this->render('dashboard/index.html.twig', [
            
+        ]);
+    }
+
+    #[Route('/dashboard/recipes', name:'dashboard_recipes')]
+    public function dash_recipes(RecipesRepository $repo ):Response
+    {
+        $recettes = $repo->findAll();
+        return $this->render('dashboard/recipes.html.twig',[
+            'recettes'=>$recettes
+        ]);
+    }
+
+    #[Route('/dashboard/users', name:'dashboard_users')]
+    public function dash_users(UserRepository $repo ):Response
+    {
+        $users = $repo->findAll();
+        return $this->render('dashboard/users.html.twig',[
+            'users'=>$users
+        ]);
+    }
+
+    #[Route('/dashboard/comments', name:'dashboard_comments')]
+    public function dash_comments(CommentsRepository $repo ):Response
+    {
+        $comments = $repo->findAll();
+        return $this->render('dashboard/comments.html.twig',[
+            'comments'=>$comments
         ]);
     }
 }
